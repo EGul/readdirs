eb18367586735a461521a2cd7e98c1d5c3740e86
sarah <evangulyas@outlook.com>
2014-12-17 16:20:45 -0500

    refactor readdirs added error support

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ function walk(arr, fn) {
 
   (function next(item) {
 
-    if (!item) return fn(results);
+    if (!item) return fn(null, results);
 
     results.push(item.split('/').slice(1).join('/'));
 
     fs.stat(item, function(err, stats) {
 
+      if (err) return fn(err);
+
       if (!stats.isDirectory()) return next(arr.shift());
 
-      readdirs(item + '/', function(content) {
+      readdirs(item + '/', function(err, content) {
+        if (err) return fn(err);
         content.forEach(function(c) { results.push(c) });
         next(arr.shift());
       });
@@ -30,10 +33,12 @@ function readdirs(path, fn) {
 
   fs.readdir(path, function(err, files) {
 
+    if (err) return fn(err);
+
     files = files.map(function(file) { return path + file });
 
-    walk(files, function(content) {
-      fn(content);
+    walk(files, function(err, content) {
+      fn(err, content);
     });
 
   });
